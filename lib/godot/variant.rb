b0df63a03c04396a74ea63737e7b61d6e877c722
ParadoxV5 <paradox.ver5@gmail.com>
2023-09-02 22:57:46 -0600

    `variant.rbs`

--- lib/godot/variant.rb
+++ lib/godot/variant.rb
@@ -2,5 +2,11 @@
 
 module Godot
   class Variant
+    #def self.allocate: () -> instance
+    #def initialize: (*variant args) -> instance
+    #def godot_send: (*variant args) -> variant
+    
+    def to_godot = self
+    def nonzero? = !zero?
   end
 end
