8997556510c5e70db1efbfa449de2deb1573a20b
ParadoxV5 <paradox.ver5@gmail.com>
2023-09-15 20:20:56 -0600

    Packed Arrays, part 1
    more than enough to enable `RubyLanguage`

--- lib/godot/array.rb
+++ lib/godot/array.rb
@@ -26,12 +26,26 @@ module Godot
       end
       
       def each(&blk)
-        return to_enum unless blk
+        return to_enum { size } unless blk
         size.times { blk.(self[_1]) }
         self
       end
     end
   end
   
+  %w[
+    Byte
+    Int32
+    Int64
+    Float32
+    Float64
+    String
+    Vector2
+    Vector3
+    Color
+  ].each_with_index do|elem_type, index|
+    klass = Class.new(Variant::AbstractArray)
+    klass::VARIANT_TYPE = 29 + index # PACKED_BYTE_ARRAY + offset
+    const_set "Packed#{elem_type}Array", klass
   end
 end
