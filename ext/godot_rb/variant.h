0969e52bb98dbfd13947029a0c5fd51d67708259
ParadoxV5 <paradox.ver5@gmail.com>
2024-04-25 22:05:36 -0600

    Create `variant_types.c.erb`

--- ext/godot_rb/variant.h
+++ ext/godot_rb/variant.h
@@ -12,6 +12,10 @@ extern rb_data_type_t godot_rb_cVariant_types[GDEXTENSION_VARIANT_TYPE_VARIANT_M
 
 // Loaded in [`variant_types.c`](variant_types.c.erb)
 extern GDExtensionInterfaceVariantGetPtrConstructor     godot_variant_get_ptr_constructor;
+extern GDExtensionInterfaceStringNameNewWithLatin1Chars godot_string_name_new_with_latin1_chars;
+extern GDExtensionInterfaceObjectMethodBindPtrcall      godot_object_method_bind_ptrcall;
+extern GDExtensionInterfaceGlobalGetSingleton           godot_global_get_singleton;
+extern GDExtensionInterfaceClassdbGetMethodBind         godot_classdb_get_method_bind;
 
 void Init_variant_types();
 void Init_variant();
